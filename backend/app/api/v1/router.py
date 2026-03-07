983cf8d3125d75bf2b11bf46b2f43ef6c88ac0fe
YadiSuarez <151183232+YadiSuarez@users.noreply.github.com>
2026-03-07 18:27:55 -0500

    endpoint speciality

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import users
+from app.api.v1.endpoints import users, catalog
 
 router = APIRouter()
 router.include_router(users.router)
+router.include_router(catalog.router)
---
+++ backend/app/services/catalog_service.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from app.db.models.nutritionist import Specialty
+import uuid
+
+class CatalogService:
+    @staticmethod
+    def get_specialists(db: Session) -> list[Specialty]:
+        return db.query(Specialty).all()
+
+    @staticmethod
+    def get_by_id(db: Session, speciality_id: int):
+        return db.query(Specialty).filter(Specialty.id == speciality_id).first()
