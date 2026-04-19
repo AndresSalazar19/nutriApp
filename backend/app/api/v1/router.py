0fb047b02b298ad65a5d08ec24c84f37cab5fb35
YadiSuarez <151183232+YadiSuarez@users.noreply.github.com>
2026-04-19 00:03:33 -0500

    appointment service

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import users, catalog, nutritionist, educational_content
+from app.api.v1.endpoints import users, catalog, nutritionist, educational_content, appointment
 
 router = APIRouter()
 router.include_router(users.router)
 router.include_router(catalog.router)
 router.include_router(nutritionist.router)
 router.include_router(educational_content.router)
+router.include_router(appointment.router)
---
+++ backend/app/schemas/appointment.py
@@ -0,0 +1,63 @@
+from pydantic import BaseModel
+from app.schemas.user  import UserResponse
+from app.schemas.nutritionist import NutritionistProfileResponse
+from datetime import date, time, datetime
+from app.db.models.appointment import AppointmentStatus, AppointmentTypeModality, AvailabilityRuleType
+from typing import Optional
+import uuid
+
+class AppointmentRequest(BaseModel):
+    patient_id: uuid.UUID
+    nutritionist_id: uuid.UUID
+    scheduled_at: datetime
+    duration_min: int = 45
+    modality: AppointmentTypeModality = AppointmentTypeModality.virtual
+    notes: Optional[str] = None
+
+class AppointmentResponse(BaseModel):
+    id: uuid.UUID
+    patient_id: uuid.UUID
+    nutritionist_id: uuid.UUID
+    scheduled_at: datetime
+    duration_min: int
+    status: AppointmentStatus
+    modality: AppointmentTypeModality
+    meeting_url: str | None = None
+    notes: str | None = None
+    cancelled_by: uuid.UUID | None = None
+    cancelled_at: str | None = None
+
+    patient: UserResponse
+    nutritionist: UserResponse 
+
+    class Config:
+        from_attributes = True
+
+
+class AppointmentUpdateRequest(BaseModel):
+    scheduled_at: Optional[datetime] = None
+    duration_min: Optional[int] = None
+    modality: Optional[AppointmentTypeModality] = None
+    notes: Optional[str] = None
+
+class AvailabilityNutritionistRequest(BaseModel):
+    rule_type: AvailabilityRuleType
+    day_of_week: int
+    specific_date: date
+    start_time: time
+    end_time: time
+    is_available: bool = True
+
+class AvailabilityNutritionistResponse(BaseModel):
+    id: uuid.UUID
+    nutritionist_id: uuid.UUID
+    rule_type: AvailabilityRuleType
+    day_of_week: int | None = None
+    specific_date: date | None = None
+    start_time: time
+    end_time: time
+    is_available: bool
+    nutritionist: NutritionistProfileResponse
+
+    class Config:
+        from_attributes = True    
