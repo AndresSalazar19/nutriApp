d54bb37b1edd192491b3300a0a901f9198e58f73
YadiSuarez <151183232+YadiSuarez@users.noreply.github.com>
2026-04-27 19:21:31 -0500

    merge appointment

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import users, catalog, nutritionist, patients, content, educational_content, appointment
+from app.api.v1.endpoints import users, catalog, nutritionist, patients, content, appointment
 
 router = APIRouter()
 router.include_router(users.router)
 router.include_router(catalog.router)
 router.include_router(nutritionist.router)
-router.include_router(educational_content.router)
 router.include_router(appointment.router)
 router.include_router(patients.router)
 router.include_router(content.router) 
