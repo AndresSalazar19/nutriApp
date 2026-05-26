352e32bc5931738f77005aadfd6b7ace545b86fb
Fabrizzio Ontaneda <faontane@espol.edu.ec>
2026-05-25 21:28:02 -0500

    cambios

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -3,4 +3,4 @@
  *   npm start  → .env.development → localhost:8000
  *   npm build  → .env.production  → 147.93.176.210:8083
  */
-export const API_URL = process.env.REACT_APP_API_URL as string;
+export const API_URL = "http://147.93.176.210:8083/api/v1"
