78574b73ddc8bc84c566d9cc337761a8c46ea426
YadiSuarez <151183232+YadiSuarez@users.noreply.github.com>
2026-06-24 22:14:09 -0500

    endpoint agregar disponibilidad

--- backend/create_tables.py
+++ backend/create_tables.py
@@ -1,9 +1,11 @@
-from app.db.base import engine, Base
-from app.db.models.user import User, Person
-from app.db.models.patient import PatientProfile, PatientHistory
+from app.db.base import Base, engine
+from app.db.models.patient import PatientHistory, PatientProfile
 
-Base.metadata.create_all(bind=engine, tables=[
-    PatientProfile.__table__,
-    PatientHistory.__table__,
-])
+Base.metadata.create_all(
+    bind=engine,
+    tables=[
+        PatientProfile.__table__,
+        PatientHistory.__table__,
+    ],
+)
 print("Tablas patient_profiles y patient_history creadas")
